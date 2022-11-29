3e90e168153dc68004f70e2b309e34b7071a7fc7
Maria <mtens17@gmail.com>
2022-11-29 19:54:50 +0000

    Move meshes to own components

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,34 @@ function App() {
   return (
     <div id="canvas-container">
       <Canvas>
-        <OrbitControls/>
+        <OrbitControls />
         <ambientLight intensity={0.1} />
         {/* shadows */}
         <directionalLight color="white" position={[0, 0, 5]} />
-        <mesh position={[2,0,0]}>
-          {/* material */}
-          <meshStandardMaterial color="magenta" />
-          {/* geometry */}
-          <boxGeometry args={[1, 2, 3]} />
-        </mesh>
-        <mesh position={[0,-2, 0]}>
-          {/* material */}
-          <meshStandardMaterial color="yellow" />
-          {/* geometry */}
-          <boxGeometry args={[5, 2, 1]} />
-        </mesh>
+        <MagentaBox/>
+        <YellowBox/>
       </Canvas>
     </div>
   )
 }
 
 export default App
+
+//
+function YellowBox(): JSX.Element {
+  return <mesh position={[0, -2, 0]}>
+    {/* material */}
+    <meshStandardMaterial color="yellow" />
+    {/* geometry */}
+    <boxGeometry args={[5, 2, 1]} />
+  </mesh>
+}
+
+function MagentaBox(): JSX.Element {
+  return (<mesh position={[2, 0, 0]}>
+    {/* material */}
+    <meshStandardMaterial color="magenta" />
+    {/* geometry */}
+    <boxGeometry args={[1, 2, 3]} />
+  </mesh>)
+}
