c2268f927bac23728947039e6129131f9cca6e68
Maria <mtens17@gmail.com>
2022-11-30 11:09:35 +0000

    Add useMemo to keep same boxes, make deterministic, improve css

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei' 
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 function App() {
-  const boxArr: JSX.Element[] = []
   const [autoRotate, setAutoRotate] = useState(false);
 
-  for (let i= 0; i<100;i++){
-    boxArr.push(<YellowBox args={randomDimension()} 
-      color={pick(palette)} 
-      position={randomPosition(10)}/>)
+  //Generate IBox[] infos only once, on first render.  after that, give back the same value each time.
+  const vars = useMemo(createBoxInfos, []);
+
+
+  interface IBox {
+    size: [number, number, number];
+    pos: [number, number, number];
+    color: string;
+    id: number;
   }
+  function createBoxInfos() {
+    const varsTemp: IBox[] = []
+    for (let i = 0; i < 1000; i++) {
+      varsTemp.push({
+        id: i,  //it's just useful to have one!
+        size: randomDimension(),
+        pos: randomPosition(10),
+        color: pick(palette),
+      });
+    }
+    return varsTemp;
+  }
+
   return (
     <>
-    <div id="canvas-container">
-    <button className = "button" onClick = {()=> setAutoRotate(!autoRotate)}>{autoRotate === true ? "Click here to stop rotation": "Click here to start rotation"}</button>
+      <div id="canvas-container">
+        <button className="button" onClick={() => setAutoRotate(!autoRotate)}>{autoRotate === true ? "Click here to stop rotation" : "Click here to start rotation"}</button>
 
-      <Canvas>
-        <OrbitControls autoRotate={autoRotate} autoRotateSpeed={20}/>
-        <ambientLight intensity={0.1} />
-        {/* shadows */}
-        <directionalLight color="white" position={[0, 0, 5]} />
-        <directionalLight color="grey" position={[-10, 2, 0]} />
-        {/* <MagentaBox/> */}
-        {boxArr}
-      </Canvas>
-    </div>
+        <Canvas>
+          <OrbitControls autoRotate={autoRotate} autoRotateSpeed={20} />
+          <ambientLight intensity={0.1} />
+          <directionalLight color="white" position={[0, 0, 5]} />
+          <directionalLight color="grey" position={[-10, 2, 0]} />
+          {vars.map(vari => <Box key={vari.id} color={vari.color} position={vari.pos} args={vari.size} />)}
+        </Canvas>
+      </div>
     </>
   )
 }
@@ -43,40 +58,29 @@ const palette = [
 function pick<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-function randomDimension(): [number, number, number]{
+function randomDimension(): [number, number, number] {
   const w = Math.random()
   const h = Math.random()
   const d = Math.random()
-  return [w,h,d]
+  return [w, h, d]
 }
-function randomPosition(range: number): [number, number, number]{
+function randomPosition(range: number): [number, number, number] {
   return [
-    (Math.random()-0.5)*range,
-    (Math.random()-0.5)*range,
-    (Math.random()-0.5)*range
+    (Math.random() - 0.5) * range,
+    (Math.random() - 0.5) * range,
+    (Math.random() - 0.5) * range
   ]
 }
 
-interface YellowBoxProps{
+interface BoxProps {
   position: [number, number, number]
   color: string
   args: [number, number, number]
 }
 
-function YellowBox(props: YellowBoxProps): JSX.Element {
+function Box(props: BoxProps): JSX.Element {
   return <mesh position={props.position}>
-    {/* material */}
-    <meshStandardMaterial color= {props.color}/>
-    {/* geometry */}
+    <meshStandardMaterial color={props.color} />
     <boxGeometry args={props.args} />
   </mesh>
-}
-
-function MagentaBox(): JSX.Element {
-  return (<mesh position={[0, 0, 0]}>
-    {/* material */}
-    <meshStandardMaterial color="magenta" />
-    {/* geometry */}
-    <boxGeometry args={[1, 2, 3]} />
-  </mesh>)
 }
